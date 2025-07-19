d2d23c74b611ff6001ce4a571d2b241828df2418
ArvindAKG14 <139096408+ArvindAKG14@users.noreply.github.com>
2025-07-19 21:55:38 +0400

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
     expo: {
       // ... other existing config
       extra: {
-        googlePlacesApiKey: "AIzaSyCjpj6qk8pYsPWaQnolrkn0sa1ovt1Xl9c", // Replace with your actual API key
+        googlePlacesApiKey: "", // Replace with your actual API key
       },
     },
   };
