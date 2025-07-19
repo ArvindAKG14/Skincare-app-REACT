31398059f62f3664ce4ab03e4be3afe5314144fb
ArvindAKG14 <139096408+ArvindAKG14@users.noreply.github.com>
2025-07-19 21:54:17 +0400

    Add files via upload

---
+++ app.config.js
@@ -0,0 +1,8 @@
+export default {
+    expo: {
+      // ... other existing config
+      extra: {
+        googlePlacesApiKey: "AIzaSyCjpj6qk8pYsPWaQnolrkn0sa1ovt1Xl9c", // Replace with your actual API key
+      },
+    },
+  };
